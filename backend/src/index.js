1c43d629ee40d2ab68de24606827b3cec8516a6e
abhay <abhaysha25@gmail.com>
2025-09-24 16:52:17 +0530

    final changes made

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,9 +34,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Serve frontend (only if backend + frontend are deployed together)
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.get(/^\/.*$/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
 // Connect DB first, then start server
